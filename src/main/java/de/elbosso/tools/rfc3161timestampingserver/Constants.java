c9e67d8d0808ba991885992e34514145c153110f
Jürgen Key <jkey@arcor.de>
2022-09-24 20:31:05 +0200

    fixes #23 code coverage sufficient (...?)

---
+++ src/main/java/de/elbosso/tools/rfc3161timestampingserver/Constants.java
@@ -0,0 +1,7 @@
+package de.elbosso.tools.rfc3161timestampingserver;
+
+public interface Constants
+{
+    java.lang.String INCLUDE_FULL_CHAIN="de.elbosso.tools.rfc3161timestampingserver.App.includeFullChain";
+    java.lang.String INCLUDE_CRLS="de.elbosso.tools.rfc3161timestampingserver.App.includeCRLs";
+}
