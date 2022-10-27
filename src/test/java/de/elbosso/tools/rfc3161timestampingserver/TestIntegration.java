d73ac327b79677143280ab4ca064228d3ca6f194
Jürgen Key <jkey@arcor.de>
2022-10-27 17:51:02 +0200

    Preparation of rest testing

--- src/test/java/de/elbosso/tools/rfc3161timestampingserver/TestIntegration.java
+++ src/test/java/de/elbosso/tools/rfc3161timestampingserver/TestIntegration.java
@@ -1,8 +1,14 @@
 package de.elbosso.tools.rfc3161timestampingserver;
 
 import io.javalin.Javalin;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.HttpStatus;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
@@ -13,12 +19,14 @@ public class TestIntegration
 {
     private final static org.slf4j.Logger CLASS_LOGGER=org.slf4j.LoggerFactory.getLogger(TestIntegration.class);
 	private final static org.slf4j.Logger EXCEPTION_LOGGER=org.slf4j.LoggerFactory.getLogger("ExceptionCatcher");
+    private final static int TEST_PORT=13456;
+
     static Javalin javalin;
     @BeforeAll
     static void setup()
     {
         Security.addProvider(new BouncyCastleProvider());
-        javalin=App.init(13456);
+        javalin=App.init(TEST_PORT);
     }
     @AfterAll
     static void tearDown()
@@ -30,8 +38,25 @@ public class TestIntegration
     @SetEnvironmentVariable(key = Constants.JDBC_PASSWORD, value = "")
     @SetEnvironmentVariable(key = Constants.JDBC_USER, value = "sa")
     @SetEnvironmentVariable(key = Constants.PERSISTENCE_UNIT_NAME, value = "PERSISTENCE_UNIT_NAME_FOR_TESTS")
-    public void test_POST_unsupportedContentType() throws Exception
+    public void test_Success() throws Exception
     {
+        HttpUriRequest request=new HttpGet( "http://localhost:"+TEST_PORT+"/hguhu");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
 
+        // Then
+        Assertions.assertEquals(HttpStatus.SC_NOT_FOUND,httpResponse.getCode());
+    }
+    @Test
+    @SetEnvironmentVariable(key = Constants.JDBC_URL, value = "jdbc:h2:mem:test")
+    @SetEnvironmentVariable(key = Constants.JDBC_PASSWORD, value = "")
+    @SetEnvironmentVariable(key = Constants.JDBC_USER, value = "sa")
+    @SetEnvironmentVariable(key = Constants.PERSISTENCE_UNIT_NAME, value = "PERSISTENCE_UNIT_NAME_FOR_TESTS")
+    public void test_Fail() throws Exception
+    {
+        HttpUriRequest request=new HttpGet( "http://localhost:"+TEST_PORT+"/hguhu");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+        // Then
+        Assertions.assertEquals(HttpStatus.SC_OK,httpResponse.getCode());
     }
 }
