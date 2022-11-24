2a1839a991f1468aa86fa109ee074941f3ea367a
Jürgen Key <jkey@arcor.de>
2022-11-24 08:32:38 +0100

    breaking tests mended

--- src/test/java/de/elbosso/tools/rfc3161timestampingserver/TestPostQuery.java
+++ src/test/java/de/elbosso/tools/rfc3161timestampingserver/TestPostQuery.java
@@ -1,6 +1,9 @@
 package de.elbosso.tools.rfc3161timestampingserver;
 
+import de.elbosso.tools.rfc3161timestampingserver.dao.DaoFactory;
+import de.elbosso.tools.rfc3161timestampingserver.dao.Rfc3161timestampDao;
 import de.elbosso.tools.rfc3161timestampingserver.domain.Rfc3161Timestamp;
+import de.elbosso.tools.rfc3161timestampingserver.domain.Rfc3161timestamp;
 import de.elbosso.tools.rfc3161timestampingserver.service.CryptoResourceManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,13 +12,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
 public class TestPostQuery
 {
     private io.javalin.http.Context ctx = mock(io.javalin.http.Context.class); // javalin 2.1.0 or before: "mock-maker-inline" must be enabled
-    private EntityManager em = mock(EntityManager.class);
+    private DaoFactory df=mock(DaoFactory.class);
+    private Rfc3161timestampDao dao=mock(Rfc3161timestampDao.class);
     private Query findYoungestByMsgImprintBase64=mock(Query.class);
     private Query findYoungestByMsgDigestAndImprintBase64=mock(Query.class);
     private Query findYoungestByMsgImprintHex=mock(Query.class);
@@ -25,7 +30,7 @@ public class TestPostQuery
     @BeforeEach
     void init()
     {
-        handlers=new Handlers(em,cryptoResourceManager);
+        handlers=new Handlers(df,cryptoResourceManager);
     }
 
     @Test
@@ -40,14 +45,12 @@ public class TestPostQuery
     @Test
     public void test_POST_to_query_no_algoId_nothingFoundInDatabase() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-data");
         when(ctx.formParam("algoid")).thenReturn(null);
         when(ctx.formParam("msgDigestBase64")).thenReturn("msgDigestBase64");
         when(ctx.formParam("msgDigestHex")).thenReturn(null);
-        when(em.createNamedQuery("Rfc3161Timestamp.findYoungestByMsgImprintBase64")).thenReturn(findYoungestByMsgImprintBase64);
-        when(findYoungestByMsgImprintBase64.setParameter(eq("Imprint"),any(String.class))).thenReturn(findYoungestByMsgImprintBase64);
-        when(findYoungestByMsgImprintBase64.setMaxResults(1)).thenReturn(findYoungestByMsgImprintBase64);
-        when(findYoungestByMsgImprintBase64.getResultList()).thenReturn(Collections.EMPTY_LIST);
+        when(dao.findYoungestByMsgImprintBase64(any(Rfc3161timestamp.class))).thenReturn(Optional.empty());
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePostQuery(ctx);
@@ -56,18 +59,14 @@ public class TestPostQuery
     @Test
     public void test_POST_to_query_no_algoId_msgDigestBase64_foundInDatabase() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-data");
         when(ctx.formParam("algoid")).thenReturn(null);
         when(ctx.formParam("msgDigestBase64")).thenReturn("msgDigestBase64");
         when(ctx.formParam("msgDigestHex")).thenReturn(null);
-        when(em.createNamedQuery("Rfc3161Timestamp.findYoungestByMsgImprintBase64")).thenReturn(findYoungestByMsgImprintBase64);
-        when(findYoungestByMsgImprintBase64.setParameter(eq("Imprint"),any(String.class))).thenReturn(findYoungestByMsgImprintBase64);
-        when(findYoungestByMsgImprintBase64.setMaxResults(1)).thenReturn(findYoungestByMsgImprintBase64);
-        java.util.List<Rfc3161Timestamp> results=new java.util.LinkedList();
-        Rfc3161Timestamp rfc3161Timestamp=new Rfc3161Timestamp();
-        rfc3161Timestamp.setTsrData(new byte[0]);
-        results.add(rfc3161Timestamp);
-        when(findYoungestByMsgImprintBase64.getResultList()).thenReturn(results);
+        Rfc3161timestamp rfc3161Timestamp=new Rfc3161timestamp();
+        rfc3161Timestamp.setTsr_data(new byte[0]);
+        when(dao.findYoungestByMsgImprintBase64(any(Rfc3161timestamp.class))).thenReturn(Optional.of(rfc3161Timestamp));
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePostQuery(ctx);
@@ -79,15 +78,12 @@ public class TestPostQuery
     @Test
     public void test_POST_to_query_algoId_msgDigestBase64_nothingFoundInDatabase() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-data");
         when(ctx.formParam("algoid")).thenReturn("algoid");
         when(ctx.formParam("msgDigestBase64")).thenReturn("msgDigestBase64");
         when(ctx.formParam("msgDigestHex")).thenReturn(null);
-        when(em.createNamedQuery("Rfc3161Timestamp.findYoungestByMsgDigestAndImprintBase64")).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.setParameter(eq("Alg"),any(String.class))).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.setParameter(eq("Imprint"),any(String.class))).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.setMaxResults(1)).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.getResultList()).thenReturn(Collections.EMPTY_LIST);
+        when(dao.findYoungestByMsgDigestAndImprintBase64(any(Rfc3161timestamp.class))).thenReturn(Optional.empty());
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePostQuery(ctx);
@@ -96,19 +92,14 @@ public class TestPostQuery
     @Test
     public void test_POST_to_query_algoId_msgDigestBase64_foundInDatabase() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-data");
         when(ctx.formParam("algoid")).thenReturn("algoid");
         when(ctx.formParam("msgDigestBase64")).thenReturn("msgDigestBase64");
         when(ctx.formParam("msgDigestHex")).thenReturn(null);
-        when(em.createNamedQuery("Rfc3161Timestamp.findYoungestByMsgDigestAndImprintBase64")).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.setParameter(eq("Alg"),any(String.class))).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.setParameter(eq("Imprint"),any(String.class))).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        when(findYoungestByMsgDigestAndImprintBase64.setMaxResults(1)).thenReturn(findYoungestByMsgDigestAndImprintBase64);
-        java.util.List<Rfc3161Timestamp> results=new java.util.LinkedList();
-        Rfc3161Timestamp rfc3161Timestamp=new Rfc3161Timestamp();
-        rfc3161Timestamp.setTsrData(new byte[0]);
-        results.add(rfc3161Timestamp);
-        when(findYoungestByMsgDigestAndImprintBase64.getResultList()).thenReturn(results);
+        Rfc3161timestamp rfc3161Timestamp=new Rfc3161timestamp();
+        rfc3161Timestamp.setTsr_data(new byte[0]);
+        when(dao.findYoungestByMsgDigestAndImprintBase64(any(Rfc3161timestamp.class))).thenReturn(Optional.of(rfc3161Timestamp));
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePostQuery(ctx);
@@ -132,14 +123,12 @@ public class TestPostQuery
     @Test
     public void test_POST_to_query_msgDigestHex_nothingFoundInDatabase() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-data");
         when(ctx.formParam("algoid")).thenReturn(null);
         when(ctx.formParam("msgDigestBase64")).thenReturn(null);
         when(ctx.formParam("msgDigestHex")).thenReturn("");
-        when(em.createNamedQuery("Rfc3161Timestamp.findYoungestByMsgImprintHex")).thenReturn(findYoungestByMsgImprintHex);
-        when(findYoungestByMsgImprintHex.setParameter(eq("Imprint"),any(String.class))).thenReturn(findYoungestByMsgImprintHex);
-        when(findYoungestByMsgImprintHex.setMaxResults(1)).thenReturn(findYoungestByMsgImprintHex);
-        when(findYoungestByMsgImprintHex.getResultList()).thenReturn(java.util.Collections.emptyList());
+        when(dao.findYoungestByMsgImprintHex(any(Rfc3161timestamp.class))).thenReturn(Optional.empty());
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePostQuery(ctx);
@@ -148,18 +137,14 @@ public class TestPostQuery
     @Test
     public void test_POST_to_query_msgDigestHex_foundInDatabase() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-data");
         when(ctx.formParam("algoid")).thenReturn(null);
         when(ctx.formParam("msgDigestBase64")).thenReturn(null);
         when(ctx.formParam("msgDigestHex")).thenReturn("");
-        when(em.createNamedQuery("Rfc3161Timestamp.findYoungestByMsgImprintHex")).thenReturn(findYoungestByMsgImprintHex);
-        when(findYoungestByMsgImprintHex.setParameter(eq("Imprint"),any(String.class))).thenReturn(findYoungestByMsgImprintHex);
-        when(findYoungestByMsgImprintHex.setMaxResults(1)).thenReturn(findYoungestByMsgImprintHex);
-        java.util.List<Rfc3161Timestamp> results=new java.util.LinkedList();
-        Rfc3161Timestamp rfc3161Timestamp=new Rfc3161Timestamp();
-        rfc3161Timestamp.setTsrData(new byte[0]);
-        results.add(rfc3161Timestamp);
-        when(findYoungestByMsgImprintHex.getResultList()).thenReturn(results);
+        Rfc3161timestamp rfc3161Timestamp=new Rfc3161timestamp();
+        rfc3161Timestamp.setTsr_data(new byte[0]);
+        when(dao.findYoungestByMsgImprintHex(any(Rfc3161timestamp.class))).thenReturn(Optional.of(rfc3161Timestamp));
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePostQuery(ctx);
