3806217f56f4a4e3395d63ca03e5b133b4aca1cd
Jürgen Key <jkey@arcor.de>
2022-11-24 08:32:38 +0100

    breaking tests mended

--- src/test/java/de/elbosso/tools/rfc3161timestampingserver/TestPost.java
+++ src/test/java/de/elbosso/tools/rfc3161timestampingserver/TestPost.java
@@ -1,6 +1,9 @@
 package de.elbosso.tools.rfc3161timestampingserver;
 
+import de.elbosso.tools.rfc3161timestampingserver.dao.DaoFactory;
+import de.elbosso.tools.rfc3161timestampingserver.dao.Rfc3161timestampDao;
 import de.elbosso.tools.rfc3161timestampingserver.domain.Rfc3161Timestamp;
+import de.elbosso.tools.rfc3161timestampingserver.domain.Rfc3161timestamp;
 import de.elbosso.tools.rfc3161timestampingserver.impl.DefaultCryptoResourceManager;
 import de.elbosso.tools.rfc3161timestampingserver.service.CryptoResourceManager;
 import io.javalin.http.UploadedFile;
@@ -21,11 +24,8 @@ import static org.mockito.Mockito.*;
 public class TestPost
 {
     private io.javalin.http.Context ctx = mock(io.javalin.http.Context.class); // javalin 2.1.0 or before: "mock-maker-inline" must be enabled
-    private EntityManager em = mock(EntityManager.class);
-    private Query findYoungestByMsgImprintBase64=mock(Query.class);
-    private Query findYoungestByMsgDigestAndImprintBase64=mock(Query.class);
-    private Query findYoungestByMsgImprintHex=mock(Query.class);
-    private EntityTransaction entityTransaction=mock(EntityTransaction.class);
+    private DaoFactory df=mock(DaoFactory.class);
+    private Rfc3161timestampDao dao=mock(Rfc3161timestampDao.class);
     private CryptoResourceManager cryptoResourceManager=mock(CryptoResourceManager.class);
     private CryptoResourceManager defCryptoResourceManager=new DefaultCryptoResourceManager();
     private Handlers handlers;
@@ -42,7 +42,7 @@ public class TestPost
     @BeforeEach
     void init() throws IOException
     {
-        handlers=new Handlers(em,cryptoResourceManager);
+        handlers=new Handlers(df,cryptoResourceManager);
         java.io.ByteArrayOutputStream baos=new java.io.ByteArrayOutputStream();
         java.io.InputStream is=de.netsysit.util.ResourceLoader.getResourceAsStream("example.tsq");
         de.elbosso.util.Utilities.copyBetweenStreams(is,baos,true);
@@ -66,18 +66,17 @@ public class TestPost
     @Test
     public void test_POST_body() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("application/timestamp-query");
         when(ctx.bodyAsBytes()).thenReturn(tsq);
-        when(em.getTransaction()).thenReturn(entityTransaction);
-        doNothing().when(entityTransaction).begin();
         when(cryptoResourceManager.getChainPem()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/chain.pem"));
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
         when(cryptoResourceManager.getPrivateKey()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.key"));
         doAnswer(invocation -> {
-            Rfc3161Timestamp arg0 = invocation.getArgument(0);
-            arg0.setId(java.math.BigInteger.valueOf(1));
+            Rfc3161timestamp arg0 = invocation.getArgument(0);
+            arg0.setId(java.math.BigDecimal.valueOf(1));
             return null;
-        }).when(em).persist(any(Rfc3161Timestamp.class));
+        }).when(dao).persist(any(Rfc3161timestamp.class));
 
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
@@ -87,21 +86,19 @@ public class TestPost
         verify(ctx).contentType("application/timestamp-reply");
         verify(ctx).header("Content-Disposition","filename=\"reply.tsr\"");
         verify(ctx).result(any(java.io.ByteArrayInputStream.class));
-        verify(entityTransaction).commit();
     }
     @Test
     public void test_POST_body_invalidRequest() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("application/timestamp-query");
         when(ctx.bodyAsBytes()).thenReturn(notsq);
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
-        when(em.getTransaction()).thenReturn(entityTransaction);
-        doNothing().when(entityTransaction).begin();
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePost(ctx);
         verify(ctx).status(500);
-        verify(entityTransaction).rollback();
+        verify(dao).rollbackTransaction();
     }
 
     @Test
@@ -118,20 +115,20 @@ public class TestPost
     @Test
     public void test_POST_formData() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-dataxyp");
         java.io.ByteArrayInputStream bais=new java.io.ByteArrayInputStream(tsq);
         UploadedFile uploadedFile=new UploadedFile(bais,"application/timestamp-query",tsq.length,"example.tsq",".tsq");
         when(ctx.uploadedFile("tsq")).thenReturn(uploadedFile);
-        when(em.getTransaction()).thenReturn(entityTransaction);
         when(cryptoResourceManager.getChainPem()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/chain.pem"));
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
         when(cryptoResourceManager.getPrivateKey()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.key"));
-        doNothing().when(entityTransaction).begin();
+        doNothing().when(dao).beginTransaction();
         doAnswer(invocation -> {
-            Rfc3161Timestamp arg0 = invocation.getArgument(0);
-            arg0.setId(java.math.BigInteger.valueOf(1));
+            Rfc3161timestamp arg0 = invocation.getArgument(0);
+            arg0.setId(java.math.BigDecimal.valueOf(1));
             return null;
-        }).when(em).persist(any(Rfc3161Timestamp.class));
+        }).when(dao).persist(any(Rfc3161timestamp.class));
 
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
@@ -140,47 +137,47 @@ public class TestPost
         verify(ctx).contentType("application/timestamp-reply");
         verify(ctx).header("Content-Disposition","filename=\"reply.tsr\"");
         verify(ctx).result(any(java.io.ByteArrayInputStream.class));
-        verify(entityTransaction).commit();
+        verify(dao).commitTransaction();
     }
     @Test
     public void test_POST_formData_invalid_request() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-dataxyp");
         java.io.ByteArrayInputStream bais=new java.io.ByteArrayInputStream(notsq);
         UploadedFile uploadedFile=new UploadedFile(bais,"application/timestamp-query",notsq.length,"example.tsq",".tsq");
         when(ctx.uploadedFile("tsq")).thenReturn(uploadedFile);
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
-        when(em.getTransaction()).thenReturn(entityTransaction);
-        doNothing().when(entityTransaction).begin();
+        doNothing().when(dao).beginTransaction();
         doAnswer(invocation -> {
             Rfc3161Timestamp arg0 = invocation.getArgument(0);
             arg0.setId(java.math.BigInteger.valueOf(1));
             return null;
-        }).when(em).persist(any(Rfc3161Timestamp.class));
+        }).when(dao).persist(any(Rfc3161timestamp.class));
 
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
         handlers.handlePost(ctx);
         verify(ctx).status(500);
-        verify(entityTransaction).rollback();
+        verify(dao).rollbackTransaction();
     }
     @Test
     public void test_POST_formData_nocert() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-dataxyp");
         java.io.ByteArrayInputStream bais=new java.io.ByteArrayInputStream(tsq_nocert);
         UploadedFile uploadedFile=new UploadedFile(bais,"application/timestamp-query",tsq_nocert.length,"example_nocert.tsq",".tsq");
         when(ctx.uploadedFile("tsq")).thenReturn(uploadedFile);
-        when(em.getTransaction()).thenReturn(entityTransaction);
-        doNothing().when(entityTransaction).begin();
+        doNothing().when(dao).beginTransaction();
         when(cryptoResourceManager.getChainPem()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/chain.pem"));
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
         when(cryptoResourceManager.getPrivateKey()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.key"));
         doAnswer(invocation -> {
-            Rfc3161Timestamp arg0 = invocation.getArgument(0);
-            arg0.setId(java.math.BigInteger.valueOf(1));
+            Rfc3161timestamp arg0 = invocation.getArgument(0);
+            arg0.setId(java.math.BigDecimal.valueOf(1));
             return null;
-        }).when(em).persist(any(Rfc3161Timestamp.class));
+        }).when(dao).persist(any(Rfc3161timestamp.class));
 
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
@@ -189,26 +186,26 @@ public class TestPost
         verify(ctx).contentType("application/timestamp-reply");
         verify(ctx).header("Content-Disposition","filename=\"reply.tsr\"");
         verify(ctx).result(any(java.io.ByteArrayInputStream.class));
-        verify(entityTransaction).commit();
+        verify(dao).commitTransaction();
     }
     @Test
     @SetEnvironmentVariable(key = Constants.INCLUDE_CRLS, value = "true")
     public void test_POST_formData_includeCRLs() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-dataxyp");
         java.io.ByteArrayInputStream bais=new java.io.ByteArrayInputStream(tsq);
         UploadedFile uploadedFile=new UploadedFile(bais,"application/timestamp-query",tsq.length,"example.tsq",".tsq");
         when(ctx.uploadedFile("tsq")).thenReturn(uploadedFile);
-        when(em.getTransaction()).thenReturn(entityTransaction);
-        doNothing().when(entityTransaction).begin();
+        doNothing().when(dao).beginTransaction();
         when(cryptoResourceManager.getChainPem()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/chain.pem"));
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
         when(cryptoResourceManager.getPrivateKey()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.key"));
         doAnswer(invocation -> {
-            Rfc3161Timestamp arg0 = invocation.getArgument(0);
-            arg0.setId(java.math.BigInteger.valueOf(1));
+            Rfc3161timestamp arg0 = invocation.getArgument(0);
+            arg0.setId(java.math.BigDecimal.valueOf(1));
             return null;
-        }).when(em).persist(any(Rfc3161Timestamp.class));
+        }).when(dao).persist(any(Rfc3161timestamp.class));
 
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
@@ -217,26 +214,26 @@ public class TestPost
         verify(ctx).contentType("application/timestamp-reply");
         verify(ctx).header("Content-Disposition","filename=\"reply.tsr\"");
         verify(ctx).result(any(java.io.ByteArrayInputStream.class));
-        verify(entityTransaction).commit();
+        verify(dao).commitTransaction();
     }
     @Test
     @SetEnvironmentVariable(key = Constants.INCLUDE_FULL_CHAIN, value = "true")
     public void test_POST_formData_includeFullChain() throws Exception
     {
+        when(df.createRfc3161timestampDao()).thenReturn(dao);
         when(ctx.contentType()).thenReturn("multipart/form-dataxyp");
         java.io.ByteArrayInputStream bais=new java.io.ByteArrayInputStream(tsq);
         UploadedFile uploadedFile=new UploadedFile(bais,"application/timestamp-query",tsq.length,"example.tsq",".tsq");
         when(ctx.uploadedFile("tsq")).thenReturn(uploadedFile);
-        when(em.getTransaction()).thenReturn(entityTransaction);
-        doNothing().when(entityTransaction).begin();
+        doNothing().when(dao).beginTransaction();
         when(cryptoResourceManager.getChainPem()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/chain.pem"));
         when(cryptoResourceManager.getTsaCert()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.crt"));
         when(cryptoResourceManager.getPrivateKey()).thenReturn(de.netsysit.util.ResourceLoader.getResource("rfc3161timestampingserver/priv/tsa.key"));
         doAnswer(invocation -> {
-            Rfc3161Timestamp arg0 = invocation.getArgument(0);
-            arg0.setId(java.math.BigInteger.valueOf(1));
+            Rfc3161timestamp arg0 = invocation.getArgument(0);
+            arg0.setId(java.math.BigDecimal.valueOf(1));
             return null;
-        }).when(em).persist(any(Rfc3161Timestamp.class));
+        }).when(dao).persist(any(Rfc3161timestamp.class));
 
         when(ctx.ip()).thenReturn("127.0.0.1");
         when(ctx.host()).thenReturn("localhost");
@@ -245,6 +242,6 @@ public class TestPost
         verify(ctx).contentType("application/timestamp-reply");
         verify(ctx).header("Content-Disposition","filename=\"reply.tsr\"");
         verify(ctx).result(any(java.io.ByteArrayInputStream.class));
-        verify(entityTransaction).commit();
+        verify(dao).commitTransaction();
     }
 }
